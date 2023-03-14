20b43c408bcd382867c10c8e9c05b5496516122b
Patricia Siuha Liu <liupatricia.18@gmail.com>
2023-03-14 06:48:39 -0400

    Add netlify url-1

--- server/src/app.js
+++ server/src/app.js
@@ -21,7 +21,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:3000",
-      "https://online-food-order-patricia.netlify.app/",
+      "https://online-food-order-patricia.netlify.app",
     ],
     credentials: true,
   })
