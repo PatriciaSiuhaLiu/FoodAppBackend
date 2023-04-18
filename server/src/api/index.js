0ddd701a2d187897bfd69005059579410d87f7c7
Patricia Siuha Liu <liupatricia.18@gmail.com>
2023-04-18 14:31:25 -0400

    cart

--- server/src/api/index.js
+++ server/src/api/index.js
@@ -2,15 +2,17 @@ const express = require("express");
 const registerApi = require("./register");
 const loginApi = require("./login");
 const paymentApi = require("./payment");
-const restaurantApi = require("./restaurantList")
-const restaurantMenuApi = require("./menuItem")
-const contact = require("./contactUs")
+const restaurantApi = require("./restaurantList");
+const restaurantMenuApi = require("./menuItem");
+const contact = require("./contactUs");
+const cartAPI = require("./cartData");
 
 const router = express.Router();
 router.use(contact);
 router.use(registerApi);
 router.use(loginApi);
 router.use(paymentApi);
-router.use(restaurantApi)
+router.use(restaurantApi);
 router.use(restaurantMenuApi);
+router.use(cartAPI);
 module.exports = router;
